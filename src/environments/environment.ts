e43e09938740b779cf5e1d49a783bccedf38068b
FishHunter1 <102452694+FishHunter1@users.noreply.github.com>
2022-05-24 19:32:30 -0500

    nice

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiURL: 'https://628d6436a339dfef879a1f93.mockapi.io/'
 };
 
 /*
